ff78fd81ff067fb6d6fe8b04c2c41d2a1319f20b
BenDing96 <dzy731500>
2019-03-12 02:39:29 -0500

    Initialized

---
+++ A4/cmake-build-debug/CMakeFiles/A4.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/BPlusTest/source/BPlusQUnit.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/BPlusTest/source/BPlusQUnit.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/BufferMgr/source/MyDB_BufferManager.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/BufferMgr/source/MyDB_BufferManager.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/BufferMgr/source/MyDB_Page.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/BufferMgr/source/MyDB_Page.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/Catalog/source/MyDB_Catalog.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/Catalog/source/MyDB_Catalog.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/Catalog/source/MyDB_Schema.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/Catalog/source/MyDB_Schema.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/Catalog/source/MyDB_Table.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/Catalog/source/MyDB_Table.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_BPlusTreeReaderWriter.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_BPlusTreeReaderWriter.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_PageListIteratorAlt.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageListIteratorAlt.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_PageReaderWriter.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageReaderWriter.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_PageRecIterator.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageRecIterator.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_PageRecIteratorAlt.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageRecIteratorAlt.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_RunQueueIteratorAlt.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_RunQueueIteratorAlt.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_TableReaderWriter.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_TableReaderWriter.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_TableRecIterator.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_TableRecIterator.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/MyDB_TableRecIteratorAlt.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_TableRecIteratorAlt.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/DatabaseTable/source/Sorting.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/DatabaseTable/source/Sorting.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/Record/source/MyDB_AttVal.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/Record/source/MyDB_AttVal.cc.o"
+  "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/Main/Record/source/MyDB_Record.cc" "/Users/dingzheyu/Desktop/DatabaseImplementation/Homework/COMP530/A4/cmake-build-debug/CMakeFiles/A4.dir/Main/Record/source/MyDB_Record.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../Build"
+  "../Build/bin"
+  "../Main"
+  "../Main/BPlusTest"
+  "../Main/BPlusTest/source"
+  "../Main/BufferMgr"
+  "../Main/BufferMgr/headers"
+  "../Main/BufferMgr/source"
+  "../Main/Catalog"
+  "../Main/Catalog/headers"
+  "../Main/DatabaseTable"
+  "../Main/DatabaseTable/headers"
+  "../Main/DatabaseTable/source"
+  "../Main/Qunit"
+  "../Main/Qunit/headers"
+  "../Main/Record"
+  "../Main/Record/headers"
+  "../Main/Record/source"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ A4/cmake-build-debug/CMakeFiles/A4.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A4.dir/Main/BPlusTest/source/BPlusQUnit.cc.o"
+  "CMakeFiles/A4.dir/Main/BufferMgr/source/MyDB_BufferManager.cc.o"
+  "CMakeFiles/A4.dir/Main/BufferMgr/source/MyDB_Page.cc.o"
+  "CMakeFiles/A4.dir/Main/Catalog/source/MyDB_Catalog.cc.o"
+  "CMakeFiles/A4.dir/Main/Catalog/source/MyDB_Schema.cc.o"
+  "CMakeFiles/A4.dir/Main/Catalog/source/MyDB_Table.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_BPlusTreeReaderWriter.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageListIteratorAlt.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageReaderWriter.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageRecIterator.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_PageRecIteratorAlt.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_RunQueueIteratorAlt.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_TableReaderWriter.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_TableRecIterator.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/MyDB_TableRecIteratorAlt.cc.o"
+  "CMakeFiles/A4.dir/Main/DatabaseTable/source/Sorting.cc.o"
+  "CMakeFiles/A4.dir/Main/Record/source/MyDB_AttVal.cc.o"
+  "CMakeFiles/A4.dir/Main/Record/source/MyDB_Record.cc.o"
+  "A4.pdb"
+  "A4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
